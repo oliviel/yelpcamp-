51d4e713f83b6f1c8cb825f982a414511cb9fdf0
oliviel <olivielultimate@gmail.com>
2018-03-26 00:27:14 -0400

    Add register logic

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// PASSPORT CONFIGURATION 
+app.use(require("express-session")({
+    secret: "the iluminatis are watching you",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function (req, res) {
     res.render("landing");
 });
@@ -102,6 +114,31 @@ app.get("/campgrounds/new", function (req, res) {
     //connect new comment to campground
     //redirect campground show page
  });
+
+
+//======================
+//  AUTH ROUTES 
+//======================
+
+// show register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+// handle sign up logic 
+app.post("/register", function (req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser,req.body.password, function (err, user) {
+        if (err) {
+            console.log("THERE IS AN ERROR");
+            console.log(err)
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+           res.redirect("/campgrounds"); 
+        });
+    });
+});
 
 app.listen(3000, function () {
     console.log("the Yelcamp server is fighting");
