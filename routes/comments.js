63c1e62bd9048c78dbe9e46391297df814cea58b
oliviel <olivielultimate@gmail.com>
2018-04-01 00:35:29 -0400

    Add hide/show edit buttons

--- routes/comments.js
+++ routes/comments.js
@@ -43,7 +43,7 @@ router.post("/", isLoggIn, function(req, res){
 });
 
 // COMMENT EDIT ROUTE 
-router.get("/:comment_id/edit", function (req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function (req, res) {
     Comment.findById(req.params.comment_id, function (err, foundComment) {
         if (err) {
             res.redirect("back");
@@ -65,7 +65,7 @@ router.put("/:comment_id", function (req, res) {
 });
 
 // COMMENT DESTORY ROUTE 
-router.delete("/:comment_id", function (req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function (req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function (err) {
         if (err) {
             res.redirect("back");
@@ -74,6 +74,25 @@ router.delete("/:comment_id", function (req, res) {
         }
     });
 });
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                // does user own the comment?
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
 
 // Middleware
 function isLoggIn(req, res, next) {
