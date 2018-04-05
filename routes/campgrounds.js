568a381c674fe993d4f2296abce876bec8144ce3
oliviel <olivielultimate@gmail.com>
2018-04-04 22:26:58 -0400

    Refactor login and register

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res) {
             console.log("THERE IS AN ERROR LOOK THE CODE SLAVE");
             console.log(err);
         } else{
-            res.render("campgrounds/index", {campgrounds: allCampgrounds});
+            res.render("campgrounds/index", {campgrounds: allCampgrounds, page: 'campgrounds'});
         }
     });
 });
